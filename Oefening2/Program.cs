0fed41ce10f229c553c34f1ab48e523033432f16
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-09-24 11:57:35 +0200

    oefeningen

---
+++ Oefening2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint weerstand1, weerstand2;
+            float vervangingsweerstand;
+            Console.WriteLine("Geef de waarden op.");
+            weerstand1 = uint.Parse(Console.ReadLine());
+            weerstand2 = uint.Parse(Console.ReadLine());
+            vervangingsweerstand = 1 / weerstand1 + 1 / weerstand2;
+            Console.WriteLine("De vervangingsweerstand is:\t {0:f2} ", 1 / vervangingsweerstand);
+        }
+    }
+}
--- Oefeningen/Program.cs
+++ Oefeningen/Program.cs
@@ -10,6 +10,14 @@ namespace Oefeningen
     {
         static void Main(string[] args)
         {
+            int intGetal1 = 20, intGetal2 = 20, intRes;
+            float floatRes;
+            intRes = intGetal1++; floatRes = ++intGetal1;
+            Console.WriteLine("res1 = {0} en res2 = {1} ", intRes, floatRes);
+            intGetal2++;
+            intRes = (intGetal1 + intGetal2) / 2;
+            floatRes = (float)((intGetal1 + intGetal2) / 2.0);
+            Console.WriteLine("res1 = {0} en res2 = {1:f2} ", intRes, floatRes);
         }
     }
 }
