41383f7ae934eb79b0a088700672f8a3084e0d58
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-09-27 11:32:30 +0200

    oef3

---
+++ Oefening3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint getal;
+            Console.WriteLine("Geef het getal in.");
+            getal = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Minuten is: {0:D}", getal / 60);
+            Console.WriteLine("Uren is: {0:D}", getal / 3600);
+        }
+    }
+}
