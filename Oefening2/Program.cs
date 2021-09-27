eb92c3f299d0593675fd9d454ddeca849ac2a915
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-09-27 10:59:48 +0200

    oef2 done

--- Oefening2/Program.cs
+++ Oefening2/Program.cs
@@ -11,12 +11,10 @@ namespace Oefening2
         static void Main(string[] args)
         {
             uint weerstand1, weerstand2;
-            float vervangingsweerstand;
             Console.WriteLine("Geef de waarden op.");
             weerstand1 = uint.Parse(Console.ReadLine());
             weerstand2 = uint.Parse(Console.ReadLine());
-            vervangingsweerstand = 1 / weerstand1 + 1 / weerstand2;
-            Console.WriteLine("De vervangingsweerstand is:\t {0:f2} ", 1 / vervangingsweerstand);
+            Console.WriteLine("De vervangingsweerstand is:\t {0:f2} ", (float)(weerstand1*weerstand2)/(weerstand1+weerstand2));
         }
     }
 }
