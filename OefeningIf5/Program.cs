8a80cc5dc3af4e282b885883bb2a2ea5716a368a
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-01 11:55:26 +0200

    Oefening If 5

---
+++ OefeningIf5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningIf5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int get1, get2;
+            Console.WriteLine("Geet 2 getallen in.");
+            get1 = int.Parse(Console.ReadLine());
+            get2 = int.Parse(Console.ReadLine());
+            Console.WriteLine((get1 > get2) ? "Getal 1 is groter." : "Getal 2 is groter.");
+            
+        }
+    }
+}
---
+++ TestOefeningen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOefeningen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = int.Parse(Console.ReadLine());
+            Console.WriteLine((getal == 1) ? "1" : "anders");
+        }
+    }
+}
