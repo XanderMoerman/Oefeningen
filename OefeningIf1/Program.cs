f9a97c7cf55ecf280b40441f1024852a26f3cc3d
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-01 10:53:19 +0200

    If oefening 1

---
+++ OefeningIf1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningIf1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint getal;
+            Console.WriteLine("Geef een positief groot getal in.");
+            getal = uint.Parse(Console.ReadLine());
+            if ((getal % 2) == 0) Console.WriteLine("Getal is even.");
+            else Console.WriteLine("Getal is oneven.");
+
+        }
+    }
+}
