e62159b77f8f77d7c9b8f2be383f805f3e22a893
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-01 11:34:44 +0200

    If oefening 4

---
+++ OefeningIf4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningIf4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte getal;
+            Console.WriteLine("Geef een getal in tussen -10 en 40.");
+            getal = sbyte.Parse(Console.ReadLine());
+            if (getal > 30 || getal < 0) Console.WriteLine("getal is extreem");
+            else Console.WriteLine("getal is normaal");
+        }
+    }
+}
