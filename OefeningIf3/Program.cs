daf580e90c0b5028a711769d58eaab4cfd73a24e
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-01 11:25:00 +0200

    If oefening 3

---
+++ OefeningIf3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningIf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte getal;
+            Console.WriteLine("Geef een klein getal op.");
+            getal = sbyte.Parse(Console.ReadLine());
+            if (getal < 0) Console.WriteLine("Getal is negatief.");
+            else if (getal > 0) Console.WriteLine("Getal is positief.");
+            else Console.WriteLine("Getal is 0");
+        }
+    }
+}
