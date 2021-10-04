3f73db2548d3b734a4190b8f15103468f2c938c6
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-04 11:21:25 +0200

    Case1

---
+++ OefeningCase1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningCase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Antwoord = Console.ReadLine();
+            switch (Antwoord)
+            {
+                case "hello": Console.WriteLine("Hallo iedereen!");
+                    break;
+                case "Goodbye": Console.WriteLine("Tot ziens!");
+                    break;
+                default: Console.WriteLine("Wat zeg je?");
+                    break;
+            }
+        }
+    }
+}
