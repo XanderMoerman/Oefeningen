84b67d3e86a74d8e955bdbd35d965d77c08e9136
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-04 11:45:13 +0200

    oef1 case

--- OefeningCase1/Program.cs
+++ OefeningCase1/Program.cs
@@ -10,14 +10,18 @@ namespace OefeningCase1
     {
         static void Main(string[] args)
         {
-            string Antwoord = Console.ReadLine();
-            switch (Antwoord)
+            float Get1, Get2;
+            string stand;
+            Console.WriteLine("Geef 2 weestands waarden.");
+            Get1 = float.Parse(Console.ReadLine());
+            Get2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Geef de stand van de weestanden: serie of parallel.");
+            stand = Console.ReadLine();
+            switch (stand)
             {
-                case "hello": Console.WriteLine("Hallo iedereen!");
+                case "serie": Console.WriteLine("De vervangingsweerstand is: {0}", Get1 + Get2);
                     break;
-                case "Goodbye": Console.WriteLine("Tot ziens!");
-                    break;
-                default: Console.WriteLine("Wat zeg je?");
+                case "parallel": Console.WriteLine("De vervangingsweerstand is: {0}", 1 / (1 / Get1) + (1 / Get2));
                     break;
             }
         }
