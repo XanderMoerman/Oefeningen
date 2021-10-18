01a547c99e7e69699d2947b974c30d7442bfbf1e
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-18 11:49:53 +0200

    oef 5 loop

---
+++ OefeningLoop5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningLoop5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float term, som = 0;
+            float aantal, teller;
+            Console.WriteLine("Berekening van 1/1 + 1/2 + ... + 1/n");
+            Console.WriteLine("Geef het aantal of n: ");
+            aantal = Convert.ToInt32(Console.ReadLine());
+            for(teller = 1; teller != aantal; ++teller)
+            {
+                term = 1 / teller;
+                som += term;
+            }
+            Console.WriteLine("de som is: {0}", som);
+        }
+    }
+}
