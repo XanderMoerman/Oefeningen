980c2354d9ac3a1ddf3fd05cb7fed60107581499
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-22 11:57:48 +0200

    extra loop

---
+++ OefeningLoopExtra1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningLoopExtra1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een getal op tussen 1 en 10");
+            byte getal = byte.Parse(Console.ReadLine());
+            int teller = 0;
+            Console.WriteLine("Vermenigvuldigingen: ");
+            while(teller != 11)
+            {
+                Console.WriteLine(getal * teller);
+                teller++;
+            }
+        }
+    }
+}
---
+++ OefeningLoopExtra2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningLoopExtra2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = int.Parse(Console.ReadLine());
+            int lijn = 1; // welke lijn hij moet schrijven.
+            while(lijn < (getal + 1))
+            {
+                int tabs = getal - lijn;
+                int teller = 0;
+                while (teller < tabs)
+                {
+                    Console.Write(".");
+                    teller++;
+                }
+                teller = 0;
+
+                Console.Write("x");
+                             
+                teller = 0;
+                while (teller < tabs)
+                {
+                    Console.Write(".");
+                    teller++;
+                }
+                Console.WriteLine("");
+                lijn++;
+            }
+        }
+    }
+}
