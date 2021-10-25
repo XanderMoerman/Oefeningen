6c98728b8e2c78886e0b52d0ca4d8341e29cf8be
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-25 11:56:41 +0200

    oef 2 string

---
+++ OefeningString2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een zin op.");
+            string str = Console.ReadLine();
+            Console.WriteLine("Geef een woord op.");
+            string str2 = Console.ReadLine();
+
+
+        }
+    }
+}
