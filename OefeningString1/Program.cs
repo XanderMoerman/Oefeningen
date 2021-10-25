cda19db1b20da7be6b07a765c17f520aa36c47d1
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-10-25 11:52:21 +0200

    str oef 1

---
+++ OefeningString1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningString1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            
+            while(str.Contains(" "))
+            {
+                str = str.Remove(str.IndexOf(" "), 1);
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
