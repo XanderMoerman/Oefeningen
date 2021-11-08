176fc6185e65f9834025f97609a1c73641c55198
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-11-08 11:21:58 +0100

    string oef 2

--- OefeningString2/Program.cs
+++ OefeningString2/Program.cs
@@ -12,10 +12,17 @@ namespace OefeningString2
         {
             Console.WriteLine("Geef een zin op.");
             string str = Console.ReadLine();
+            string newStr = str;
             Console.WriteLine("Geef een woord op.");
             string str2 = Console.ReadLine();
-
-
+            short count = 0;
+            while (newStr.Contains(str2))
+            {
+                newStr = newStr.Remove(newStr.IndexOf(str2), str2.Length);
+                count++;
+            }
+            Console.WriteLine("Het woord komt er {0} keer in.", count);
+            Console.WriteLine("Zin: {0}\t New zin: {1}", str, newStr);
         }
     }
 }
