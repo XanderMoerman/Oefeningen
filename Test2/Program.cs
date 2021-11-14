c219eab62cf514448540c4e7a8fd86fc199f4d30
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-11-14 12:26:59 +0100

    test2

---
+++ Test2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleSpiner spin = new ConsoleSpiner();
+            Console.Write("Working... ");
+            while (true)
+            {
+                spin.Turn();
+            }
+        }
+
+        public class ConsoleSpiner
+        {
+            int counter;
+            public ConsoleSpiner()
+            {
+                counter = 0;
+            }
+            public void Turn()
+            {
+                counter++;
+                switch (counter % 4)
+                {
+                    case 0: Console.Write("/"); break;
+                    case 1: Console.Write("-"); break;
+                    case 2: Console.Write("\\"); break;
+                    case 3: Console.Write("|"); break;
+                }
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            }
+        }
+    }
+}
