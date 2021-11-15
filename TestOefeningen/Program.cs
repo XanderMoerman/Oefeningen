84b431ba414d7c749acc2648c736036072b31722
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-11-15 09:34:28 +0100

    Random

--- TestOefeningen/Program.cs
+++ TestOefeningen/Program.cs
@@ -10,7 +10,10 @@ namespace TestOefeningen
     {
         static void Main(string[] args)
         {
-            
+            Random rnd1 = new Random();
+            int Top = rnd1.Next(1, 10);
+            int Low = rnd1.Next(1, 10);
+            Console.WriteLine("Waardes: {0}, {1}", Top, Low);
         }
     }
 }
