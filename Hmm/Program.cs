59f0385159ea571f4fc5424d9e02790a1cb1fc92
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-11-15 09:02:31 +0100

    Thread.Sleep()

---
+++ Hmm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Hmm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een waarde in.");
+            int waarde = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Geef een snelheid in.(in ms)");
+            int snelheid = int.Parse(Console.ReadLine());
+            for (int counter = 0; waarde >= counter; ++counter)
+            {
+                Console.Clear();
+                Console.WriteLine(counter);
+                Thread.Sleep(snelheid);
+            }
+        }
+    }
+}
