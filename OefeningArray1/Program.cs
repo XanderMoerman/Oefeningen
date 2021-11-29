084f2c2933d31c573d9f80dc0dbb9ae9efcce25d
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-11-29 11:56:39 +0100

    array 1 en 2

---
+++ OefeningArray1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningArray1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var rand = new Random();
+            int[] array1 = new int[5];
+            int[] array2 = new int[5];
+            int[] array3 = new int[5];
+            for (int i = 0; i < array1.Length; ++i)
+            {
+                array1[i] = rand.Next(0, 10);
+                array2[i] = rand.Next(0, 10);
+                Console.WriteLine("Array1: {0}\tArray2: {1}", array1[i], array2[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array3.Length; ++i)
+            {
+                array3[i] = array1[i] + array2[i];
+                Console.WriteLine("Array3: {0}", array3[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OefeningArray2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningArray2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef 20 waarden in en een 0 om te stoppen.");
+            int stopWaarde = 0;
+            string antwoord = "";
+            int[] array1 = new int[20];
+            for (int i = 0; i < array1.Length; ++i)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+                if (array1[i] == 0 || array1[i] == array1.Length)
+                {
+                    stopWaarde = Array.IndexOf(array1, 0);
+                    break;
+                }
+            }
+            Console.WriteLine("StopWaarde is: {0}", stopWaarde);
+            Console.WriteLine("Druk op een knop om verder te gaan.");
+            Console.Read();
+            Console.Clear();
+            while(antwoord != "stop")
+            {
+                Console.WriteLine("gem = bereken het Gemiddelde,\t som = bereken de Som,\t groot = bereken het grootste getal,\t stop = sluit af.");
+                antwoord = Console.ReadLine();
+                //Gemiddelde
+                if(antwoord == "gem")
+                {
+                    int som = 0;
+                    for(int i = 0; i < stopWaarde; ++i)
+                    {
+                        som += array1[i];
+                    }
+                    Console.WriteLine("Het gemiddelde is: {0}", som / stopWaarde);
+                }
+
+                //som
+                if (antwoord == "som")
+                {
+                    int som = 0;
+                    for (int i = 0; i < stopWaarde; ++i)
+                    {
+                        som += array1[i];
+                    }
+                    Console.WriteLine("De som is: {0}", som);
+                }
+            }
+        }
+    }
+}
