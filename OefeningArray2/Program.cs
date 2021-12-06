a1ad69f3be69e75f9d596e953b04319bff7f2d18
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2021-12-06 11:58:26 +0100

    array oef 2

--- OefeningArray2/Program.cs
+++ OefeningArray2/Program.cs
@@ -20,6 +20,7 @@ namespace OefeningArray2
                 if (array1[i] == 0 || array1[i] == array1.Length)
                 {
                     stopWaarde = Array.IndexOf(array1, 0);
+                    if (array1[i] == array1.Length) stopWaarde = 20;
                     break;
                 }
             }
@@ -27,12 +28,13 @@ namespace OefeningArray2
             Console.WriteLine("Druk op een knop om verder te gaan.");
             Console.Read();
             Console.Clear();
-            while(antwoord != "stop")
+            while (antwoord != "stop")
             {
+                Console.Clear();
                 Console.WriteLine("gem = bereken het Gemiddelde,\t som = bereken de Som,\t groot = bereken het grootste getal,\t stop = sluit af.");
                 antwoord = Console.ReadLine();
                 //Gemiddelde
-                if(antwoord == "gem")
+                if (antwoord == "gem")
                 {
                     int som = 0;
                     for(int i = 0; i < stopWaarde; ++i)
@@ -40,6 +42,8 @@ namespace OefeningArray2
                         som += array1[i];
                     }
                     Console.WriteLine("Het gemiddelde is: {0}", som / stopWaarde);
+                    System.Threading.Thread.Sleep(2000);
+                    Console.Clear();
                 }
 
                 //som
@@ -51,8 +55,26 @@ namespace OefeningArray2
                         som += array1[i];
                     }
                     Console.WriteLine("De som is: {0}", som);
+                    System.Threading.Thread.Sleep(2000);
+                    Console.Clear();
+                }
+
+                //groot
+                if (antwoord == "groot")
+                {
+                    int groot = 0;
+                    for (int i = 0; i < stopWaarde; ++i)
+                    {
+                        if (array1[i] > groot) groot = array1[i];
+                    }
+                    Console.WriteLine("Het grootste getal is: {0}", groot);
+                    System.Threading.Thread.Sleep(2000);
+                    Console.Clear();
                 }
             }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Program stopped.");
         }
     }
 }
