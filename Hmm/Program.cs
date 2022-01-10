1e6cd1920003b8196d6f6223a48bc9bcfc5e32c7
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-01-10 11:57:24 +0100

    oef 3

--- Hmm/Program.cs
+++ Hmm/Program.cs
@@ -10,17 +10,7 @@ namespace Hmm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Geef een waarde in.");
-            int waarde = int.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("Geef een snelheid in.(in ms)");
-            int snelheid = int.Parse(Console.ReadLine());
-            for (int counter = 0; waarde >= counter; ++counter)
-            {
-                Console.Clear();
-                Console.WriteLine(counter);
-                Thread.Sleep(snelheid);
-            }
+            
         }
     }
 }
--- OefeningArray3/Program.cs
+++ OefeningArray3/Program.cs
@@ -12,13 +12,22 @@ namespace OefeningArray3
         {
             Console.WriteLine("Geef 20 waarden in en een -1 om te stoppen.");
             int stopWaarde = -1;
-            int aantal = 0;
+            int aantal = 1;
             int[] array1 = new int[20];
             for (int i = 0; i < array1.Length; ++i)
             {
                 array1[i] = int.Parse(Console.ReadLine());
                 if (array1[i] == stopWaarde) break;
-                if (array1.Contains(array1[i])) aantal++;
+                for(int k = 0; k != Array.IndexOf(array1, array1[i]); k++)
+                {
+                    if (array1[i] == array1[k]) continue;
+                    else
+                    {
+                        aantal++;
+                        break;
+                    }
+                }
+                Console.WriteLine("aantal = {0}", aantal);
             }
             Console.WriteLine("Er zijn {0} aantal verschillende getallen.", aantal);
                     
