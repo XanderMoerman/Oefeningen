a15a80cea780d126077586488680c4dc3ce4a11b
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-01-10 11:33:56 +0100

    array Oefening 3

--- OefeningArray3/Program.cs
+++ OefeningArray3/Program.cs
@@ -10,7 +10,18 @@ namespace OefeningArray3
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Geef 20 waarden in en een -1 om te stoppen.");
+            int stopWaarde = -1;
+            int aantal = 0;
+            int[] array1 = new int[20];
+            for (int i = 0; i < array1.Length; ++i)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+                if (array1[i] == stopWaarde) break;
+                if (array1.Contains(array1[i])) aantal++;
+            }
+            Console.WriteLine("Er zijn {0} aantal verschillende getallen.", aantal);
+                    
 
         }
     }
