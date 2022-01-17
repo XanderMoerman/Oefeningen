038198eaeb50eeb1b575a0a8ecf7ab31e5e2542f
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-01-17 11:17:16 +0100

    galgje 2.0

---
+++ Galgje 2.0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galgje_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een woord in.");
+            string word = Console.ReadLine();
+            string letter;
+            string lines;
+            word = word.ToLower();
+            lines = word;
+            for(int i = 0; i < word.Length; ++i)
+            {
+                lines = lines.Replace(word[i], '_');
+            }
+            Console.Clear();
+            for(int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine(lines);
+                Console.WriteLine("Geef een letter in.");
+                letter = Console.ReadLine();
+                letter = letter.ToLower();
+                if (word.Contains(letter))
+                {
+                    while (word.Contains(letter))
+                    {
+                        word = word.Remove(word.IndexOf(letter), 1);
+                        Console.WriteLine(word);
+                        System.Threading.Thread.Sleep(500);
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+}
