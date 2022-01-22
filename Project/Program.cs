53abc4ebe68441a82fe0c3d72ac1b213825a7ef8
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-01-22 13:53:21 +0100

    project1 done

--- Project/Program.cs
+++ Project/Program.cs
@@ -16,77 +16,84 @@ namespace Project
                 Console.WriteLine("Welke soort code wil je?\nGeef 1 voor BCD.\nGeef 2 voor decimaal.\nGeef 0 om te eindigen.");
                 soort = short.Parse(Console.ReadLine());
                 string code;
-
-                if (soort == 1) // keuze is BDC
+                switch (soort)
                 {
-                    Console.WriteLine("Geef je BCD code in van 8 karakters.");
-                    code = Console.ReadLine();
-                    if(code.Length != 8) //als de waarde geen 8 karakters bevat, geef error
-                    {
-                        Console.WriteLine("Error, geef een BDC waarde in van 8 karakters.");
-                        System.Threading.Thread.Sleep(2000);
-                    }
-                    else
-                    {
-                        Console.WriteLine("De ingegeven BCD {0} naar andere codes:", code);
-                        //Decimal
-                        string split1 = code.Remove(4);
-                        string split2 = code.Remove(0, 4);
-                        int dec1 = Convert.ToInt32(split1, 2);
-                        int dec2 = Convert.ToInt32(split2, 2);
-                        string dec = Convert.ToString(dec1) + Convert.ToString(dec2);
-                        int number = int.Parse(dec);
-                        Console.WriteLine("De decimale code: {0}", number);
+                    case 1:
+                        {
+                            Console.WriteLine("Geef je BCD code in van 8 karakters.");
+                            code = Console.ReadLine();
+                            if (code.Length != 8) //als de waarde geen 8 karakters bevat, geef error
+                            {
+                                Console.WriteLine("Error, geef een BDC waarde in van 8 karakters.");
+                                Console.WriteLine();
+                                System.Threading.Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                Console.WriteLine("De ingegeven BCD {0} naar andere codes:", code);
+                                //Decimal
+                                string split1 = code.Remove(4);
+                                string split2 = code.Remove(0, 4);
+                                int dec1 = Convert.ToInt32(split1, 2);
+                                int dec2 = Convert.ToInt32(split2, 2);
+                                string dec = Convert.ToString(dec1) + Convert.ToString(dec2);
+                                int number = int.Parse(dec);
+                                Console.WriteLine("De decimale code: {0}", number);
 
-                        //Hex
-                        string hex = Convert.ToString(number, 16);
-                        Console.WriteLine("De Hexadecimale code: 0X{0}", hex);
+                                //Hex
+                                string hex = Convert.ToString(number, 16);
+                                Console.WriteLine("De Hexadecimale code: 0X{0}", hex);
 
-                        //binair
-                        int bin1 = Convert.ToInt32(hex, 16);
-                        string binair = Convert.ToString(bin1, 2);
-                        Console.WriteLine("Binair: 0b{0}", binair);
+                                //binair
+                                int bin1 = Convert.ToInt32(hex, 16);
+                                string binair = Convert.ToString(bin1, 2);
+                                Console.WriteLine("Binair: 0b{0}", binair);
 
-                        //Ascii
-                        Console.WriteLine("ASCII waarde: {0}", (char)number);
-                    }
-                    
+                                //Ascii
+                                Console.WriteLine("ASCII waarde: {0}", (char)number);
 
-                }
-                if (soort == 2) //keuze is decimaal
-                {
-                    Console.WriteLine("Geef de decimale waarde:");
-                    code = Console.ReadLine();
-                    int number = int.Parse(code);
-                    if (number > 99 || number < 0) //als de waarde kleiner dan 0 of groter dan 99 is, geef error.
-                    {
-                        Console.WriteLine("Error, Geef een waarde in tusssen 0 en 99.");
-                        System.Threading.Thread.Sleep(2000);
-                    }
-                    else
-                    {
-                        Console.WriteLine("De decimale code {0} geconverteerd naar andere codes:", code);
-                        //hex
-                        string hex = Convert.ToString(number, 16);
-                        Console.WriteLine("De Hex code: 0x{0}", hex);
+                                Console.WriteLine();
+                            }
+                        }break;
 
-                        //binair
-                        int bin1 = Convert.ToInt32(hex, 16);
-                        string binair = Convert.ToString(bin1, 2);
-                        Console.WriteLine("Binair: 0b{0}", binair);
+                    case 2:
+                        {
+                            Console.WriteLine("Geef de decimale waarde:");
+                            code = Console.ReadLine();
+                            int number = int.Parse(code);
+                            if (number > 99 || number < 0) //als de waarde kleiner dan 0 of groter dan 99 is, geef error.
+                            {
+                                Console.WriteLine("Error, Geef een waarde in tusssen 0 en 99.");
+                                Console.WriteLine();
+                                System.Threading.Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                Console.WriteLine("De decimale code {0} geconverteerd naar andere codes:", code);
+                                //hex
+                                string hex = Convert.ToString(number, 16);
+                                Console.WriteLine("De Hex code: 0x{0}", hex);
 
-                        //ASCII
-                        Console.WriteLine("ASCII waarde: {0}", (char)number);
+                                //binair
+                                int bin1 = Convert.ToInt32(hex, 16);
+                                string binair = Convert.ToString(bin1, 2);
+                                Console.WriteLine("Binair: 0b{0}", binair);
 
-                        //BDC
-                        string split1 = code.Remove(1, 1);
-                        string split2 = code.Remove(0, 1);
-                        int dec1 = Convert.ToInt32(split1, 8);
-                        int dec2 = Convert.ToInt32(split2, 8);
-                        string BCD1 = Convert.ToString(dec1, 2);
-                        string BCD2 = Convert.ToString(dec2, 2);
-                        Console.WriteLine("BCDD code: {0} {1}", BCD1, BCD2);
-                    }
+                                //ASCII
+                                Console.WriteLine("ASCII waarde: {0}", (char)number);
+
+                                //BDC
+                                string split1 = code.Remove(1, 1);
+                                string split2 = code.Remove(0, 1);
+                                int dec1 = Convert.ToInt32(split1, 8);
+                                int dec2 = Convert.ToInt32(split2, 8);
+                                string BCD1 = Convert.ToString(dec1, 2);
+                                string BCD2 = Convert.ToString(dec2, 2);
+                                Console.WriteLine("BCDD code: {0} {1}", BCD1, BCD2);
+
+                                Console.WriteLine();
+                            }
+                        }break;
                 }
             }
             //keuze is 0, stop programma.
