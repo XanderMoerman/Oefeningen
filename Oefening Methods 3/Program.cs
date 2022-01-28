7e4a4741718bd8764c852ef1789085e39e386845
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-01-28 11:22:42 +0100

    Methods Oef 3

---
+++ Oefening Methods 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_Methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef 2 getalln in.");
+            int get1 = int.Parse(Console.ReadLine());
+            int get2 = int.Parse(Console.ReadLine());
+            int getal = Berekening(get1, get2);
+            Console.WriteLine("Verschil is: {0}", getal);
+        }
+        static int Berekening(int get1, int get2)
+        {
+            return get1 - get2;
+        }
+    }
+}
