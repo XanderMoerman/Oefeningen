5201725b171b632211b13ee989c78faa58a88162
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-02-04 11:55:05 +0100

    Galgje 2.0 half done

--- Galgje 2.0/Program.cs
+++ Galgje 2.0/Program.cs
@@ -12,44 +12,35 @@ namespace Galgje_2._0
         {
             Console.WriteLine("Geef een woord in.");
             string word = Console.ReadLine();
-            char letter;
-            string lines;
+            string StartWord = word;
+            string letter;
             word = word.ToLower();
-            lines = word;
-            for(int i = 0; i < word.Length; ++i)
-            {
-                lines = lines.Replace(word[i], '_'); //plaats steepjes
-            }
             Console.Clear();
             for(int i = 0; i < 5; ++i) // Main loop, tot 5
             {
+                if (word == "") break; // als woord is geraden, break de loop
                 Console.Clear();
-                Console.WriteLine(lines);
                 Console.WriteLine("Geef een letter in.");
-                letter = char.Parse(Console.ReadLine());
-                if (word.Contains(letter))
+                letter = Console.ReadLine();
+                if (word.Contains(letter)) //als de ingelezen letter in het woord zit
                 {
                     Console.WriteLine("Letter zit in het woord.");
-                    lines = word;
-                    while (word.Contains(letter))
-                    {
-                        for(int k = 0; i < word.Length; ++i)
-                        {
-                            if(lines[k] == letter)
-                            {
-                                // hier
-                            }
-                        }
-                        word = word.Remove(word.IndexOf(letter), 1);
-                    }
-                    System.Threading.Thread.Sleep(500);
+                    word = word.Remove(word.IndexOf(letter), 1); //de letter word uit het woord verwijdert
+                    i--; //teller gaat omlaag met 1
+                    System.Threading.Thread.Sleep(500); // wacht 500ms
                 }
-                else if (!word.Contains(letter))
+                else if (!word.Contains(letter)) // anders als de ingelezen letter niet in het woord zit
                 {
                     Console.WriteLine("Letter zit niet in het woord.");
                     System.Threading.Thread.Sleep(500);
                 }
+                
             }
+            Console.Clear();
+            Console.WriteLine("Het woord was: {0}", StartWord);
+            if (word == "") Console.WriteLine("Woord geraden");
+            else Console.WriteLine("Probeer nog een keer.");
+            System.Threading.Thread.Sleep(500);
         }
     }
 }
