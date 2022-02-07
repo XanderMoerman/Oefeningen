1eea6acbf81ff19506deb45bb82e00af7417db01
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-02-07 11:49:17 +0100

    FoutOef2

--- FoutOef2/Program.cs
+++ FoutOef2/Program.cs
@@ -10,6 +10,13 @@ namespace FoutOef2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int i = int.Parse("2147498545");
+            }
+            catch (System.OverflowException e) { Console.WriteLine(e.Message); } //eerst een overflow als de waarde te groot of te klein is.
+            catch (System.ArithmeticException e) { Console.WriteLine(e.Message); } // dan deze als de parse niet goed verlopen is
+            catch (System.Exception e) { Console.WriteLine(e.Message); } // daarna de andere indien er zijn
         }
     }
 }
