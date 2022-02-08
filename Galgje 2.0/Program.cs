6d2b40886cd174dbf455727b3ad027db7b8c7b67
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-02-08 11:52:50 +0100

    Galgje 2.0

--- Galgje 2.0/Program.cs
+++ Galgje 2.0/Program.cs
@@ -14,20 +14,32 @@ namespace Galgje_2._0
             string word = Console.ReadLine();
             string StartWord = word;
             string letter;
+            string lines = "";
             word = word.ToLower();
             Console.Clear();
+            for(int i = 0; i < word.Length; ++i)
+            {
+                lines = lines.Insert(i, "*");
+            }
             for(int i = 0; i < 5; ++i) // Main loop, tot 5
             {
                 if (word == "") break; // als woord is geraden, break de loop
                 Console.Clear();
+                Console.WriteLine(lines);
                 Console.WriteLine("Geef een letter in.");
                 letter = Console.ReadLine();
                 if (word.Contains(letter)) //als de ingelezen letter in het woord zit
                 {
                     Console.WriteLine("Letter zit in het woord.");
-                    word = word.Remove(word.IndexOf(letter), 1); //de letter word uit het woord verwijdert
-                    i--; //teller gaat omlaag met 1
-                    System.Threading.Thread.Sleep(500); // wacht 500ms
+                    while (word.Contains(letter))
+                    {
+                        lines = lines.Insert(word.IndexOf(letter), letter);
+                        lines = lines.Remove(word.IndexOf(letter) - 1,1);
+                        word = word.Remove(word.IndexOf(letter), 1); //de letter word uit het woord verwijdert
+                        i--; //teller gaat omlaag met 1
+                        System.Threading.Thread.Sleep(500); // wacht 500ms
+                    }
+                    
                 }
                 else if (!word.Contains(letter)) // anders als de ingelezen letter niet in het woord zit
                 {
