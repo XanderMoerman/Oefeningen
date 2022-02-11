be3218c4ea1c189f3f5c9a9ab2a60183eeeb2a7a
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-02-11 11:16:38 +0100

    Oef DateTime

---
+++ OefeningDateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Druk op [ENTER] om de starten.");
+            DateTime eerste;
+            Console.ReadLine();
+            eerste = DateTime.Now;
+            Console.WriteLine("Druk op [ENTER] om te stoppen.");
+            DateTime tweede;
+            Console.ReadLine();
+            tweede = DateTime.Now;
+            TimeSpan tijd = new TimeSpan(tweede.Ticks - eerste.Ticks);
+            Console.WriteLine("{0} seconden\nEn {1} milliseconden", tijd.Seconds, tijd.Milliseconds - tijd.Seconds);
+        }
+    }
+}
