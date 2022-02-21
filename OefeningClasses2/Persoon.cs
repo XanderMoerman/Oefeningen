08cf04b5394159beb1e19956a57d32ede4ae7629
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-02-21 11:57:39 +0100

    Classes

---
+++ OefeningClasses2/Persoon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningClasses
+{
+    class Persoon
+    {
+        public string naam { get; set; } // init naam
+        private string voornaam; // veld naam
+        public string Voornaam // init voornaam
+        {
+            get { return voornaam; }
+            set { voornaam = value; }
+        }
+        public Persoon() //contructor zonder parameters
+        {
+            naam = "Onbekend";
+            voornaam = "Onbekend";
+        }
+        public Persoon(string Naam, string Voornaam) // met parameters
+        {
+            this.naam = Naam;
+            this.Voornaam = Voornaam;
+        }
+        public string VolledigeNaam() // return
+        {
+            return (naam + " " +  Voornaam);
+        }
+    }
+}
---
+++ OefeningClasses2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoon pers1 = new Persoon("Ik", "Jij");
+            Console.WriteLine(pers1.VolledigeNaam());
+            Persoon pers2 = new Persoon();
+            Console.WriteLine(pers2.VolledigeNaam());
+        }
+    }
+}
