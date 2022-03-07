e37ee45936532009b19626b8e577b7e9b6d62de4
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-03-07 11:24:15 +0100

    LeeftijdBerekenen

---
+++ Leeftijdfuncties/Leeftijd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeftijdfuncties
+{
+    public class Leeftijd
+    {
+        public static double BerekenLeeftijd (DateTime geboortedatum)
+        {
+            TimeSpan leeftijdSpan = DateTime.Now.Subtract(geboortedatum);
+            return leeftijdSpan.TotalDays / 365;
+        }
+    }
+}
