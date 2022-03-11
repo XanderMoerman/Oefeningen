f72f44a1b5b32a41158f04514774cfc731c978c0
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-03-11 11:58:57 +0100

    K8055test

---
+++ LibK8055/K8055.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibK8055
+{
+    public class K8055
+    {
+        [DllImport("K8055.dll")]
+        public static extern int OpenDevice(long CardAdress);
+        [DllImport("K8055.dll")]
+        public static extern void CloseDevice();
+        [DllImport("K8055.dll")]
+        public static extern void SetAllDigital();
+    }
+}
---
+++ TestK8055/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibK8055;
+
+namespace TestK8055
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            K8055.OpenDevice(0);
+            K8055.SetAllDigital();
+            K8055.CloseDevice();
+        }
+    }
+}
