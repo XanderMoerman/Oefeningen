dc4a233d553b091eaeb754f1407586af2f65d108
Xander Moerman <xander.moerman@sgsintpaulus.eu>
2022-05-02 11:15:25 +0200

    gegevenopslag oefeningen

---
+++ GegevenOpslag Oefening 1/MenuClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GegevenOpslag_Oefening_1
+{
+    class MenuClass
+    {
+        public static string KeuzeMenu()
+        {
+            Console.WriteLine("Een nieuwe tekst maken (N)\nTekst toevoegen aan een bestaande tekst (T)\nEen bestaande tekst wissen (W)\nToon de inhoud (I)\nStoppen met werken (S)");
+            string keuze = Console.ReadLine();
+            keuze = keuze.ToLower();
+            return keuze;
+        }
+
+        public static void BewaarBestand(string bestandsNaam, bool toevoegen, string tekst)
+        {
+
+        }
+
+        public static string ToonBestand(string bestandsNaam)
+        {
+
+        }
+    }
+}
---
+++ GegevenOpslag Oefening 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GegevenOpslag_Oefening_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keuze = "";
+            while (keuze != "s")
+            {
+                keuze = MenuClass.KeuzeMenu();
+                if (keuze == "n")
+                {
+
+                }
+            }
+        }
+
+        
+    }
+}
